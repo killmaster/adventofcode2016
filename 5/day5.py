03660fffce61dffef34503115b04ba181d55bf7d
Carlos Martins <carlosmartins8@gmail.com>
2016-12-05 22:31:06 +0000

    day5 with hacking animation

--- 5/day5.py
+++ 5/day5.py
@@ -1,9 +1,16 @@
 doorid = 'ffykfhsq'
 #doorid = 'abc'
 import hashlib
+import random
+import sys
 
 hashed = []
 index = 0
+
+def hacking_in_progress(password, hashed, index):
+    if index % 30000:
+        sys.stdout.write('password: {} cracking hash: {}\r'.format(''.join(password),hashed))
+        sys.stdout.flush()
 
 def valid(h):
     if h.startswith('00000'):
@@ -22,6 +29,7 @@ def part1(doorid):
     password = ''
     result = doorid
     while len(password) < 8:
+        hacking_in_progress(password, result, index)
         result = hashlib.md5((doorid+str(index)).encode()).hexdigest()
         if valid(result):
             password += result[5]
@@ -34,9 +42,11 @@ def part2(doorid):
     password = ['-1','-1','-1','-1','-1','-1','-1','-1']
     result = doorid
     for h in hashed:
+        hacking_in_progress(password,result,index)
         if inbounds(h) and password[int(h[5])] == '-1':
             password[int(h[5])] = h[6]
     while '-1' in password:
+        hacking_in_progress(password, result, index)
         result = hashlib.md5((doorid+str(index)).encode()).hexdigest()
         if valid(result) and inbounds(result) and password[int(result[5])] == '-1':
             password[int(result[5])] = result[6]
