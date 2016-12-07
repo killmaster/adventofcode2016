2e0cca85cbedba09db4982a62b3269cae3211346
Carlos Martins <carlosmartins8@gmail.com>
2016-12-07 07:05:42 +0000

    day7 part1 done

--- 7/day7.py
+++ 7/day7.py
@@ -1,17 +1,16 @@
 import re
 import collections
-p1 = re.compile(r'(?P<first>\w+)\[(?P<second>\w+)\](?P<third>\w+)')
+#p1 = re.compile(r'(\w+)\[(\w+)\](\w+)')
 
-with open('input-test.txt') as f:
+with open('input.txt') as f:
     lines = f.read().splitlines()
 
 def isPalindrome(word):
-    print(word)
     return word == word[::-1]
 
 def abba(word):
     index = 4
-    while index < len(word):
+    while index < len(word)+1:
         if isPalindrome(word[index-4:index]) and collections.Counter(word[index-4:index]).most_common()[0][1] != 4:
             return True
         index += 1
@@ -20,14 +19,27 @@ def abba(word):
 def part1(lines):
     result = 0
     for line in lines:
-        first = p1.match(line).group('first')
-        #print(first)
-        second = p1.match(line).group('second')
-        #print(second)
-        third = p1.match(line).group('third')
-        #print(third)
-        if abba(first) or abba(third) and not abba(second):
+        hypernet = []
+        nothypernet = []
+        hypernet_flag = False
+        nothypernet_flag = False
+        while ']' in line:
+            nothypernet.append(line[:line.find('[')])
+            line = line[line.find('[') + 1:]
+            hypernet.append(line[:line.find(']')])
+            line = line[line.find(']') + 1:]
+            if len(line) > 0:
+                nothypernet.append(line)
+        for c in hypernet:
+            if abba(c):
+                hypernet_flag = True
+        if not hypernet_flag:
+            for c in nothypernet:
+                if abba(c):
+                    nothypernet_flag = True
+        if not hypernet_flag and nothypernet_flag:
             result += 1
+
     return result
 
 print(part1(lines))
