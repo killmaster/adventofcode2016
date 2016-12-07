84ed1546c177e0057fc26b5540dde45de95fe58b
Carlos Martins <carlosmartins8@gmail.com>
2016-12-07 09:06:36 +0000

    day7 part2 done in python

--- 7/day7.py
+++ 7/day7.py
@@ -19,13 +19,13 @@ def abba(word):
 def parse(line):
     hypernet = []
     nothypernet = []
-    while ']' in line:
+    while '[' in line:
         nothypernet.append(line[:line.find('[')])
         line = line[line.find('[') + 1:]
         hypernet.append(line[:line.find(']')])
         line = line[line.find(']') + 1:]
-        if len(line) > 0:
-            nothypernet.append(line)
+    if len(line) > 0:
+        nothypernet.append(line)
     return hypernet, nothypernet
 
 
@@ -44,6 +44,8 @@ def part1(lines):
             if len(line) > 0:
                 nothypernet.append(line)"""
         hypernet, nothypernet = parse(line)
+        #print('hypernet: {}'.format(hypernet))
+        #print('nothypernet: {}'.format(nothypernet))
         for c in hypernet:
             if abba(c):
                 hypernet_flag = True
@@ -57,14 +59,48 @@ def part1(lines):
 
 def aba(word):
     index = 3
+    result = []
+    flag = False
     while index < len(word)+1:
-        if isPalindrome(word[index-3:index]) and collections.Counter(word[index-3:index]).most_common()[0][1] != 2:
-            return word[index-3:index]
+        if isPalindrome(word[index-3:index]) and collections.Counter(word[index-3:index]).most_common()[0][1] < 3:
+            result.append(word[index-3:index])
+            flag = True
         index += 1
+    if flag:
+        return result
     return False
 
 def bab(word, aba):
+    #print('bab word: {}'.format(aba))
     index = 3
+    tofind = aba[1] + aba[0] + aba[1]
     while index < len(word)+1:
-        if isPalindrome(word[index-3:index]) and collections.Counter(word[index-3:index]).most_common()[0][1] != 2 and word[index-3:index] != aba:
+        currentword = word[index-3:index]
+        if currentword == tofind:
+            #print('bab currentword: {}'.format(currentword))
+            #print('bab tofind: {}'.format(tofind))
+            return True
+        index += 1
+    return False
+
+def part2(lines):
+    result = 0
+    for line in lines:
+        found = False
+        hypernet, nothypernet = parse(line)
+        #print('hypernet: {}'.format(hypernet))
+        #print('nothypernet: {}'.format(nothypernet))
+        for c in nothypernet:
+            temp = aba(c)
+            if temp:
+                for t in temp:
+                    for h in hypernet:
+                        if bab(h, t):
+                            found = True
+        if found:
+            #print('found {}'.format(line))
+            result += 1
+    return result
+
 print(part1(lines))
+print(part2(lines))
