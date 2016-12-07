9e0139a892449263fcad43ab6b8ba486c724a70c
Carlos Martins <carlosmartins8@gmail.com>
2016-12-07 07:47:07 +0000

    small pause

--- 7/day7.py
+++ 7/day7.py
@@ -16,20 +16,34 @@ def abba(word):
         index += 1
     return False
 
+def parse(line):
+    hypernet = []
+    nothypernet = []
+    while ']' in line:
+        nothypernet.append(line[:line.find('[')])
+        line = line[line.find('[') + 1:]
+        hypernet.append(line[:line.find(']')])
+        line = line[line.find(']') + 1:]
+        if len(line) > 0:
+            nothypernet.append(line)
+    return hypernet, nothypernet
+
+
 def part1(lines):
     result = 0
     for line in lines:
-        hypernet = []
-        nothypernet = []
+        #hypernet = []
+        #nothypernet = []
         hypernet_flag = False
         nothypernet_flag = False
-        while ']' in line:
+        """while ']' in line:
             nothypernet.append(line[:line.find('[')])
             line = line[line.find('[') + 1:]
             hypernet.append(line[:line.find(']')])
             line = line[line.find(']') + 1:]
             if len(line) > 0:
-                nothypernet.append(line)
+                nothypernet.append(line)"""
+        hypernet, nothypernet = parse(line)
         for c in hypernet:
             if abba(c):
                 hypernet_flag = True
@@ -39,7 +53,18 @@ def part1(lines):
                     nothypernet_flag = True
         if not hypernet_flag and nothypernet_flag:
             result += 1
-
     return result
 
+def aba(word):
+    index = 3
+    while index < len(word)+1:
+        if isPalindrome(word[index-3:index]) and collections.Counter(word[index-3:index]).most_common()[0][1] != 2:
+            return word[index-3:index]
+        index += 1
+    return False
+
+def bab(word, aba):
+    index = 3
+    while index < len(word)+1:
+        if isPalindrome(word[index-3:index]) and collections.Counter(word[index-3:index]).most_common()[0][1] != 2 and word[index-3:index] != aba:
 print(part1(lines))
